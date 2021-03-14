7f09da38a9eb60bbc5a59d1e614149db6b13f28c
ArtIPok <apokora@itsap.pl>
2021-03-14 20:43:37 +0100

    Add cards

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,7 +3,7 @@ import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
 import Icon from '../Icon/Icon.js';
-import {settings} from '../../data/dataStore';
+// import {settings} from '../../data/dataStore';
 // import Creator from '../Creator/Creator.js';
 
 class Column extends React.Component {
@@ -14,6 +14,8 @@ class Column extends React.Component {
 
   static propTypes = {
     title: PropTypes.node,
+    icon: PropTypes.Icon,
+    cards: PropTypes.array,
   }
 
   render() {
@@ -21,9 +23,9 @@ class Column extends React.Component {
 
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.title}
+        <h3 className={styles.title}>{ title }
           <span className={styles.icon}>
-            <Icon name={this.state.cards.icon} />
+            <Icon name={ icon } />
           </span>
         </h3>
         <div className={styles.cards}>
@@ -31,7 +33,7 @@ class Column extends React.Component {
             <Card key={cardData.id} {...cardData} />
           ))}
         </div>
-        {/*<div>
+        {/* <div>
           {this.state.cards.map(({key, ...cardProps}) => (
             <Card key={key} {...cardProps} />
           ))}
