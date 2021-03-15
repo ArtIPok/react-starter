2f580567449ddc9c3d65a1df64fcb5712735557e
ArtIPok <apokora@itsap.pl>
2021-03-15 22:41:10 +0100

    Add filter step3

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -1,17 +1,25 @@
+import shortid from 'shortid';
+
 // selectors
 export const getSearchString = ({searchString}) => searchString;
 export const countAllCards = ({cards}) => cards.length;
 export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
 
 // action name creator
+const reducerName = 'filters';
+const createActionName = name => `app/${reducerName}/${name}`;
 
-// actions types
+// action types
+export const  USE_FILTR = createActionName('USE_FILTR');
 
 // action creators
+export const createActionUseFiltr = payload => ({ payload: { ...payload, id: shortid.generate() }, type: USE_FILTR });
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case USE_FILTR:
+      return [...statePart, action.payload];
     default:
       return statePart;
   }
