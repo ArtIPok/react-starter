d6c0c41885a710a3c6f8ba5ab64ab1df35632b84
ArtIPok <apokora@itsap.pl>
2021-03-16 20:51:33 +0100

    Add Container

---
+++ src/components/Container/Container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Container.scss';
+
+const Container = (children) => {
+  return (
+    <div className={styles.component}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
