7b20b277efa3ed1446b4198f375d46a6246dc1f9
ArtIPok <apokora@itsap.pl>
2021-03-16 22:21:05 +0100

    Add Navigation

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -3,8 +3,6 @@ import React from 'react';
 import styles from './Container.scss';
 
 const Container = ({children}) => {
-  // eslint-disable-next-line react/prop-types
-  // const {children} = props;
   
   return (
     <div className={styles.component}>
